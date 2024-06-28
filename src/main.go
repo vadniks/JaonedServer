4d297955c608efe0e1ed19f66228ffc0563a00b3
vadniks <whatever@fake.some>
2024-06-28 14:17:49 +0300

    Structure & logic

--- src/main.go
+++ src/main.go
@@ -16,13 +16,21 @@ func main() {
         connection, err := listener.Accept()
         Assert(err == nil)
 
-        count, err := connection.Write([]byte("Hello"))
-        Assert(count == 5 && err == nil)
+        count, err := connection.Write(make([]byte, 1000000))
+        Assert(count == 1000000 && err == nil)
 
-        reply := make([]byte, 5)
-        count, err = connection.Read(reply)
-        Assert(count == 5 && err == nil)
-        println(string(reply))
+        reply := make([]byte, 1000000)
+        count = 0
+        for {
+            xCount, err := connection.Read(reply)
+            Assert(xCount > 0 && err == nil)
+            println("read", xCount)
+
+            count += xCount
+            if count == 1000000 { break }
+        }
+
+        println(reply[:5])
 
         Assert(connection.Close() == nil)
         acceptingClients = false
