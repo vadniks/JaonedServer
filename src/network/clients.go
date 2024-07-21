28cf0dc636f1664c68f21c2801bb13c23fbb10e5
vadniks <whatever@fake.some>
2024-07-21 13:37:18 +0300

    Structure & logic

--- src/network/clients.go
+++ src/network/clients.go
@@ -12,11 +12,15 @@ type Clients interface {
     addClient(connection net.Conn, client *Client)
     getClient(connection net.Conn) *Client // nillable
     removeClient(connection net.Conn) bool
+    clientHasMessages(connection net.Conn) bool
+    enqueueMessageToClient(connection net.Conn, message *Message)
+    dequeueMessageToClient(connection net.Conn) *Message // nillable
 }
 
 type ClientsImpl struct {
     clients map[net.Conn]*Client
     rwMutex sync.RWMutex
+    pendingMessages map[net.Conn][]*Message
 }
 
 type Client struct {
@@ -32,6 +36,7 @@ func createClients() Clients {
     return &ClientsImpl{
         make(map[net.Conn]*Client),
         sync.RWMutex{},
+        make(map[net.Conn][]*Message),
     }
 }
 
@@ -58,4 +63,24 @@ func (impl *ClientsImpl) removeClient(connection net.Conn) bool {
 
     impl.rwMutex.Unlock()
     return found
+}
+
+func (impl *ClientsImpl) clientHasMessages(connection net.Conn) bool {
+    return len(impl.pendingMessages[connection]) > 0
+}
+
+func (impl *ClientsImpl) enqueueMessageToClient(connection net.Conn, message *Message) {
+    if impl.pendingMessages[connection] == nil {
+        impl.pendingMessages[connection] = make([]*Message, 0)
+    }
+
+    impl.pendingMessages[connection] = append(impl.pendingMessages[connection], message)
+}
+
+func (impl *ClientsImpl) dequeueMessageToClient(connection net.Conn) *Message { // nillable
+    if impl.pendingMessages[connection] == nil { return nil }
+
+    message := impl.pendingMessages[connection][0]
+    impl.pendingMessages[connection] = impl.pendingMessages[connection][1:]
+    return message
 }
